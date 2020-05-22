9912f07f23c58d4476b4932c45267ea3c7082a0f
Quinton Fults <coding.banjo@gmail.com>
2020-05-22 11:57:48 -0700

    code

--- routes/groceryRoutes.js
+++ routes/groceryRoutes.js
@@ -1,15 +1,22 @@
 const router = require('express').Router()
+const db = require('../db')
 
 // Grocery routes...
 
 // GET all groceries
 router.get('/groceries', (req, res) => {
-  res.send('GET all groceries')
+  db.query('SELECT * FROM groceries', (err, groceries) => {
+    if (err) { console.log(err) }
+    res.json(groceries)
+  })
 })
 
 // POST a grocery
 router.post('/groceries', (req, res) => {
-  res.send('POST a grocery')
+  db.query('INSERT INTO groceries SET ?', req.body, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+  })
 })
 
 module.exports = router
